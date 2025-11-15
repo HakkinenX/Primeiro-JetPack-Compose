aa6f1528835637acc072a3877cc76a2cb90b461f
Hakkinen <hakkinenwylgner@gmail.com>
2025-11-14 21:25:26 -0300

    Funcionalidades Surface

--- app/src/main/java/com/example/seilabrow/MainActivity.kt
+++ app/src/main/java/com/example/seilabrow/MainActivity.kt
@@ -1,21 +1,20 @@
 package com.example.seilabrow
 
 import android.os.Bundle
-import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,36 +23,43 @@ import com.example.seilabrow.ui.theme.SeilaBrowTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SeilaBrowTheme {
-
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                 }
             }
         }
     }
-
-@Composable
-fun GreetingText(modifier: Modifier = Modifier, mensagem: String){
-    Text(
-
-        text = mensagem,
-            color = Color.Red,
-        fontSize = 100.sp,
-        lineHeight = 70.sp
-
-
-    )
 }
-
-
+@Composable
+fun GreetingText(mensagem: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = mensagem,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
 @Preview(showBackground = true,)
 @Composable
-
-
-
 fun GreetingPreview() {
     SeilaBrowTheme {
-        GreetingText(mensagem = "Feliz Aniversário")
+        GreetingText(mensagem = "Feliz Aniversário", from = "From Emma")
     }
 }
