5f7c2890e77176bf8ccd40d37e9f6758904aa244
Hakkinen <hakkinenwylgner@gmail.com>
2025-11-16 12:33:30 -0300

    Image contentDescription contentScale

--- app/src/main/java/com/example/seilabrow/MainActivity.kt
+++ app/src/main/java/com/example/seilabrow/MainActivity.kt
@@ -3,24 +3,31 @@ package com.example.seilabrow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.seilabrow.R.drawable.androidparty
 import com.example.seilabrow.ui.theme.SeilaBrowTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,37 +35,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SeilaBrowTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-
-
+                    color = MaterialTheme.colorScheme.background
                 ) {
+
+                    // Aqui chamo o conteúdo que vai na tela
+                    GreetingImage()
                 }
+
             }
         }
     }
 }
+
 @Composable
-fun GreetingText(mensagem: String, from: String, modifier: Modifier = Modifier) {
+
+fun GreetingText(mensagem: String,
+                 from: String,
+                 modifier: Modifier = Modifier
+) {
     Column(
         verticalArrangement = Arrangement.Center,
         modifier = modifier
     ) {
-
         Text(
             text = mensagem,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
+            fontSize = 90.sp,
+            lineHeight = 80.sp,
             textAlign = TextAlign.Center
-
         )
         Text(
             text = from,
             fontSize = 36.sp,
             modifier = Modifier
-                .padding(16.dp).background(Color.Blue).padding(10.dp)
+                .padding(16.dp)
+                .background(Color.Blue)
+                .padding(10.dp)
                 .align(alignment = Alignment.End)
         )
         Button(
@@ -69,14 +82,42 @@ fun GreetingText(mensagem: String, from: String, modifier: Modifier = Modifier) 
             ),
             modifier = Modifier.padding(16.dp)
         ) {
-            Text("Pressione aqui")
+            Text("Botão")
         }
     }
 }
-@Preview(showBackground = true,)
+
+
+@Composable
+fun GreetingImage(
+    mensagem: String = "Feliz Aniversário",
+    from: String = "From Emma",
+    modifier: Modifier = Modifier.fillMaxSize()
+) {
+    val image = painterResource(R.drawable.androidparty)
+
+    Box(modifier) {
+
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+
+        )
+        GreetingText(
+            mensagem = mensagem,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SeilaBrowTheme {
-        GreetingText(mensagem = "Feliz Aniversário", from = "From Emma")
+        GreetingImage()
     }
 }
